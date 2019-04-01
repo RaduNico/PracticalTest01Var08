595b20901adc62d920057afe45cadfdf57353a44
Root USO User <student@stud.acs.upb.ro>
2019-04-01 13:51:52 +0300

    Application

---
+++ MyApplication/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var08/MainActivity.java
@@ -0,0 +1,67 @@
+package ro.pub.cs.systems.eim.practicaltest01var08;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText riddle, answer;
+    private Button play;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private CallImageButtonClickListener listener = new CallImageButtonClickListener();
+    private class CallImageButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_LONG).show();
+            Intent myIntent = new Intent(MainActivity.this, PlayActivity.class);
+            myIntent.putExtra("riddle", riddle.getText()); //Optional parameters
+            myIntent.putExtra("answer", answer.getText()); //Optional parameters
+            MainActivity.this.startActivity(myIntent);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        riddle = findViewById(R.id.Riddle);
+        answer = findViewById(R.id.Answer);
+        play = findViewById(R.id.Play);
+        play.setOnClickListener(listener);
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ MyApplication/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var08/PlayActivity.java
@@ -0,0 +1,51 @@
+package ro.pub.cs.systems.eim.practicaltest01var08;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+
+public class PlayActivity extends AppCompatActivity {
+    private String theAnswer;
+    private EditText myAnswer;
+    private TextView theRiddle;
+    private Button back, check;
+
+    private CallCheckListener checkListener = new CallCheckListener();
+    private class CallCheckListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if(myAnswer.getText().equals(theAnswer)) {
+                Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Wrong!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_activity);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        theRiddle = findViewById(R.id.theRiddle);
+        myAnswer = findViewById(R.id.myAnswer);
+        check = findViewById(R.id.Check);
+        check.setOnClickListener(checkListener);
+//        back = findViewById(R.id.Back);
+
+        Intent intent = getIntent();
+        theRiddle.setText(intent.getStringExtra("riddle"));
+        String theAnswer = intent.getStringExtra("answer");
+    }
+
+}
